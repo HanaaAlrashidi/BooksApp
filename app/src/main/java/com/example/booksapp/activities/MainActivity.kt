0261417d0f5f2e376bd2f434b9a22a2d59e9e6c4
hanaa <hana.alrashidi.dmm.k1@tuwaiq.edu.sa>
2021-12-09 10:24:38 +0300

    Initial commit

---
+++ app/src/main/java/com/example/booksapp/activities/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.booksapp.activities
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.booksapp.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
